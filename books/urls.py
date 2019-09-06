363c52d0c86a84e19f64260a2e713502f0f85d01
Rajagopal <rajagopal@testpress.in>
2019-09-06 18:05:35 +0530

    Include publisher views
    List view to list all publishers
    Create view to create new publisher
    Edit view to edit publisher details
    Delete view to delete publisher

--- books/urls.py
+++ books/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .book_views import books_list
 from .genre_views import GenreCreateView, GenreListView, GenreUpdateView, GenreDeleteView
 from .author_views import AuthorCreateView, AuthorListView, AuthorUpdateView, AuthorDeleteView
+from .publisher_views import PublisherCreateView, PublisherListView, PublisherUpdateView, PublisherDeleteView
 
 
 app_name = 'books'
@@ -17,4 +18,8 @@ urlpatterns = [
     path('author/create/', AuthorCreateView.as_view(), name='books-create-author'),
     path('author/<int:id>/edit/', AuthorUpdateView.as_view(), name='books-update-author'),
     path('author/<int:id>/delete/', AuthorDeleteView.as_view(), name='books-delete-author'),
+    path('publisher/', PublisherListView.as_view(), name='books-publisher-list'),
+    path('publisher/create/', PublisherCreateView.as_view(), name='books-create-publisher'),
+    path('publisher/<int:id>/edit/', PublisherUpdateView.as_view(), name='books-update-publisher'),
+    path('publisher/<int:id>/delete/', PublisherDeleteView.as_view(), name='books-delete-publisher'),
 ]
