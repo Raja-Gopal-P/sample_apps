e63bffe370ba20a74269729cb61b5476635a727a
Rajagopal <rajagopal@testpress.in>
2019-09-09 10:13:14 +0530

    Add Movie List and Detail view

--- movies/urls.py
+++ movies/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .genre_views import GenreListView, GenreDetailView
 from .studio_views import StudioListView, StudioDetailView
 from .director_views import DirectorListView, DirectorDetailView
+from .movie_views import MovieListView, MovieDetailView
 
 
 app_name = 'movies'
@@ -14,4 +15,6 @@ urlpatterns = [
     path('studio/<slug:slug>', StudioDetailView.as_view(), name='studio-detail-view'),
     path('director/', DirectorListView.as_view(), name='director-list'),
     path('director/<int:id>', DirectorDetailView.as_view(), name='director-detail-view'),
+    path('', MovieListView.as_view(), name='movie-list'),
+    path('<slug:slug>/', MovieDetailView.as_view(), name='movie-detail-view'),
 ]
